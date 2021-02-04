f2eda142834ab1ae6fb1a115473864ca363f2191
LnG <pleasewaitlong@gmail.com>
2021-02-04 10:55:16 +0700

    add files

---
+++ btvn/TriangleArea.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <cmath>
+#include <iomanip>
+
+using namespace std;
+int main()
+{
+	double a, b, c;
+	cin >> a >> b >> c;
+	double p = (a + b + c) / 2;
+	cout << fixed << setprecision(2) << sqrt((p - a) * (p - b) * (p - c) * p);
+}
