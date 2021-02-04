7ff8464987e988a32d0a2ea4ebf063feffad57f3
LnG <pleasewaitlong@gmail.com>
2021-02-04 16:43:25 +0700

    Create bai16.cpp

---
+++ btvn/bai16.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <cmath>
+#include <iomanip>
+
+using namespace std;
+int main()
+{
+	int a, b, c;
+	cin >> a >> b >> c;
+	if (a == b && b == c) cout << "true";
+	else cout << "false";
+	return 0;
+}
+
